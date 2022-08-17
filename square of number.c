0c1a681ca67c3e195cc0e9a1e425758adc2e4219
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2022-08-17 13:56:30 +0545

    Add files via upload

---
+++ square of number.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a;
+    printf("enter any number: ");
+    scanf("%d", &a);
+    a = pow(a, 2);
+    printf("the square is %d", a);
+
+    return 0;
+}

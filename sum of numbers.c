7943fc5dc96119ffbf4f79b5fed5171e15efa858
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2022-08-17 13:21:06 +0545

    Add files via upload

---
+++ sum of numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    int a, b, sum;
+    printf("enter value of a: ");
+    scanf("%d", &a);
+    printf("enter value of b: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("the total sum is : %d", sum);
+    return 0;
+}
